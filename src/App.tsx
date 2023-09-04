1a1a9b13beff09c2458cb43476706c5a5d1f633f
Felipe Gabriel da Silva <felipegabriel1106@outlook.com>
2023-09-04 09:48:50 -0300

    Retirando navbar e rodape, colocando no formulario

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import './App.css';
 import Empresa from './pages/Empresa';
-import Navbar from './components/cabecalho/cabecalho';
-import Footer from './components/rodape/rodape';
 
 function App() {
 
   return (   
     <>
-    <Navbar />
     <Empresa />
-    <Footer />
     </>
   )
 }
--- src/pages/Empresa.tsx
+++ src/pages/Empresa.tsx
@@ -1,44 +1,76 @@
 import {
-     Button,
+     Button, Input,FormControl, FormLabel, RadioGroup, Radio,
    } from '@chakra-ui/react';
+import { yupResolver } from '@hookform/resolvers/yup';
    // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { SubmitHandler, useForm, useFormState, Controller } from 'react-hook-form';
-import Formulario from '../components/formulario.tsx/formulario';
-import Parceiro from '../components/parceiro/parceiro';
+import * as yup from 'yup';
+import Navbar from '../components/cabecalho/cabecalho';
+import Footer from '../components/rodape/rodape';
     
    function Empresa() {
+    const schema = yup.object().shape({
+      nomeEmpresa: yup.string().required(),
+      cnpj: yup.string().required(),
+      descricao: yup.string().required(),
+      endereco: yup.string().required(),
+      email: yup.string().email().required(),
+      parceiro: yup.boolean().required(),
+    });
    
-     const {register, handleSubmit: onSubmit, watch, formState: {errors}, } = useForm();
+     const {register, handleSubmit: onSubmit, watch, formState: {errors}, } = useForm({
+      resolver: yupResolver(schema),
+     });
    
-     const handleSubmit = (data: object) => {
-       console.log(data);
-     }
-   
+    const handleSubmit = (data: object) => {
+      console.log(data);
+    }
+
+    
+    
      return (   
        <>
+        <Navbar/>
        <form onSubmit={onSubmit(handleSubmit)}>
-         <Formulario id="nomeEmpresa" titulo='Nome da empresa' tipo="text"
-         {...register("nomeEmpresa", {required: true})}/>
+         <FormControl id="nomeEmpresa" mt='16px'>
+            <FormLabel>Nome da empresa:</FormLabel>
+            <Input type="text" {...register("nomeEmpresa", {required: true})} />
+         </FormControl>
 
-         <Formulario id="cnpj" titulo='CNPJ da Empresa' tipo="text"
-         {...register("cnpj", {required: true})}/>
-   
-        <Formulario id="descricao" titulo='Descrição da empresa' tipo="text"
-         {...register("descricao", {required: true})}/>
-   
-        <Formulario id="endereco" titulo='Endereço da empresa' tipo="text"
-         {...register("endereco", {required: true})}/>
-   
-        <Formulario id="email" titulo='Email da empresa' tipo="email"
-         {...register("email", {required: true})}/>
-   
-        <Parceiro id="parceiro" titulo="Parceira" {...register("parceiro", {required: true})} />
+         <FormControl id="cnpj" mt='16px'>
+            <FormLabel>CNPJ da empresa:</FormLabel>
+            <Input type="text" {...register("cnpj", {required: true})} />
+         </FormControl>
+
+         <FormControl id="descricao" mt='16px'>
+            <FormLabel>Descrição da empresa</FormLabel>
+            <Input type="text" {...register("descricao", {required: true})} />
+         </FormControl>
+         
+         <FormControl id="endereco" mt='16px'>
+            <FormLabel>Endereço da empresa:</FormLabel>
+            <Input type="text" {...register("endereco", {required: true})} />
+         </FormControl>
+
+         <FormControl id="email" mt='16px'>
+            <FormLabel>E-mail da empresa:</FormLabel>
+            <Input type="email" {...register("email", {required: true})} />
+         </FormControl>
+
+         <FormControl id='parceiro' mt='16px'>
+           <FormLabel>Parceiro:</FormLabel>
+           <RadioGroup defaultValue='' display="flex" gap="16px">
+             <Radio value="true" {...register('parceiro', {required: true})}>Sim</Radio>
+             <Radio value="false" {...register('parceiro', {required: true})}>Não</Radio>
+           </RadioGroup>
+         </FormControl>
    
         <Button type='submit'
          m="auto"
          display="flex"
          mt="20px">Cadastrar empresa</Button>
        </form>
+       <Footer />
        </>
      )
    }
